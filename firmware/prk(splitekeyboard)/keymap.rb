47a32f00375d15f546d6f0d2cb64b2a7019b1935
木村 光輝 <67196597+telzo2000@users.noreply.github.com>
2022-03-09 21:29:20 +0900

    fix

--- firmware/prk(splitekeyboard)/keymap.rb
+++ firmware/prk(splitekeyboard)/keymap.rb
@@ -30,7 +30,7 @@ kbd.add_layer :default, %i[
   KC_Q     KC_W     KC_E        KC_R      KC_T     KC_Y    KC_U    KC_I   KC_O   KC_P
   KC_A     KC_S     KC_D        KC_F      KC_G     KC_H    KC_J    KC_K   KC_L   KC_MINUS
   KC_Z     KC_X     KC_C        KC_V      KC_B     KC_N    KC_M    KC_COMMA  KC_DOT   KC_ENTER      
-  KC_LSFT  KC_LCTL  KC_LGUI     KC_LALT   KC_SPACE KC_SPACE KC_1   KC_2   KC_3   KC_4
+  KC_LSFT  KC_LCTL  KC_LGUI     KC_LALT   KC_SPACE KC_SPACE KC_1   KC_2   KC_3   KC_BSPACE
 ]
 
 #
