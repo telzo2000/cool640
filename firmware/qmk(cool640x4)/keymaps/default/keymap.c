2adfcc00b695b23a4add3e415d2d702467b888e0
木村 光輝 <67196597+telzo2000@users.noreply.github.com>
2022-07-30 11:16:44 +0900

    fix

--- firmware/qmk(cool640x4)/keymaps/default/keymap.c
+++ firmware/qmk(cool640x4)/keymaps/default/keymap.c
@@ -25,10 +25,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,               KC_U,     KC_I,     KC_O,    KC_P,    
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,               KC_J,     KC_K,     KC_L,    KC_MINUS,    
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,               KC_M,     KC_COMM,  KC_DOT,  KC_ENT, 
+    KC_LCTL, KC_LGUI, KC_LALT, LT(_LOWER,KC_SPACE),KC_SPACE,LT(_RAISE,KC_SPACE),KC_LEFT, KC_DOWN, KC_UP, LT(KC_RSFT,KC_RIGHT),
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,               KC_U,     KC_I,     KC_O,    KC_P,    
+    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,               KC_J,     KC_K,     KC_L,    KC_MINUS,    
+    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,               KC_M,     KC_COMM,  KC_DOT,  KC_ENT, 
     KC_LCTL, KC_LGUI, KC_LALT, LT(_LOWER,KC_SPACE),KC_SPACE,LT(_RAISE,KC_SPACE),KC_LEFT, KC_DOWN, KC_UP, LT(KC_RSFT,KC_RIGHT)
   ),
   
   [_LOWER] = LAYOUT(
+    KC_1,     KC_2,      KC_3,      KC_4,      KC_5,                KC_6,                KC_7,     KC_8,          KC_9,           KC_0,    
+    KC_EXLM,  KC_AT,     KC_HASH,   KC_DLR,    KC_PERC,             KC_CIRC,             KC_AMPR,  KC_ASTR,       KC_LPRN,        KC_RPRN, 
+    KC_PPLS,  KC_PMNS,   KC_PAST,   KC_PSLS,   KC_PEQL,             KC_GRV,              KC_JYEN,  RSFT(KC_LBRC), RSFT(KC_RBRC),  RSFT(KC_MINS),
+    KC_TAB,   KC_INSERT, KC_SPACE,  KC_SPACE,  LT(_LOWER,KC_SPACE), LT(_RAISE,KC_SPACE), KC_LCTL,  KC_LGUI,       KC_LALT,        KC_BSPC,
     KC_1,     KC_2,      KC_3,      KC_4,      KC_5,                KC_6,                KC_7,     KC_8,          KC_9,           KC_0,    
     KC_EXLM,  KC_AT,     KC_HASH,   KC_DLR,    KC_PERC,             KC_CIRC,             KC_AMPR,  KC_ASTR,       KC_LPRN,        KC_RPRN, 
     KC_PPLS,  KC_PMNS,   KC_PAST,   KC_PSLS,   KC_PEQL,             KC_GRV,              KC_JYEN,  RSFT(KC_LBRC), RSFT(KC_RBRC),  RSFT(KC_MINS),
@@ -37,6 +45,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  
   
   [_RAISE] = LAYOUT(
+    KC_F1,    KC_F2,      KC_F3,      KC_F4,          KC_F5,               KC_F6,               KC_F7,    KC_F8,     KC_F9,     KC_F10,    
+    KC_AT,    KC_BSLS,    KC_SCLN,    RSFT(KC_SCLN),  KC_QUOT,             KC_LEFT,             KC_DOWN,  KC_UP,     KC_RIGHT,  KC_EQL,
+    RGB_TOG,  RGB_MOD,    RGB_HUI,    RGB_HUD,        RGB_SAI,             RGB_SAD,             RGB_VAI,  KC_LBRC,   KC_RBRC,   KC_BSLASH,
+    KC_TAB,   KC_INSERT,  KC_SPACE,   KC_SPACE,       LT(_LOWER,KC_SPACE), LT(_RAISE,KC_SPACE), KC_LCTL,  KC_LGUI,   KC_LALT,   KC_BSPC,
     KC_F1,    KC_F2,      KC_F3,      KC_F4,          KC_F5,               KC_F6,               KC_F7,    KC_F8,     KC_F9,     KC_F10,    
     KC_AT,    KC_BSLS,    KC_SCLN,    RSFT(KC_SCLN),  KC_QUOT,             KC_LEFT,             KC_DOWN,  KC_UP,     KC_RIGHT,  KC_EQL,
     RGB_TOG,  RGB_MOD,    RGB_HUI,    RGB_HUD,        RGB_SAI,             RGB_SAD,             RGB_VAI,  KC_LBRC,   KC_RBRC,   KC_BSLASH,
