9c7d9508dd15aa681a717d7b4a6ce9be74297ae0
木村 光輝 <67196597+telzo2000@users.noreply.github.com>
2024-04-13 22:17:36 +0900

    fix

--- firmware/qmk(0.23_splitekeyboard)/cool640/keymaps/via/keymap.c
+++ firmware/qmk(0.23_splitekeyboard)/cool640/keymaps/via/keymap.c
@@ -4,21 +4,11 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
-     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │
-     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
-     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │
-     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
-     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │
-     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
-     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │
-     * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
-     */
+
     [0] = LAYOUT_ortho_4x10(
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J
+        KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,
+        KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_ENT,
+        KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_RSFT,
+        KC_ESC, KC_TAB, KC_LCTL,KC_LGUI,KC_SPC, KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT, KC_BSPC
     )
 };
